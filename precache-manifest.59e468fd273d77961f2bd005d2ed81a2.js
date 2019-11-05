340c79bcd812047af3ee9855e695398add64c7fc
MiguelPortillo18 <00043818@uca.edu.sv>
2019-11-04 23:43:28 -0600

    Updates

---
+++ precache-manifest.59e468fd273d77961f2bd005d2ed81a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688fd229d3ad8aae97c69ca7955e065e",
+    "url": "/registroLabos/index.html"
+  },
+  {
+    "revision": "63e4ea5a1d49daa334af",
+    "url": "/registroLabos/static/css/main.8263e3f6.chunk.css"
+  },
+  {
+    "revision": "52d63b62b76d524d1f7b",
+    "url": "/registroLabos/static/js/2.02861fc9.chunk.js"
+  },
+  {
+    "revision": "63e4ea5a1d49daa334af",
+    "url": "/registroLabos/static/js/main.4ff8bd95.chunk.js"
+  },
+  {
+    "revision": "ac07d8bd0e7c0decab0e",
+    "url": "/registroLabos/static/js/runtime-main.f254067d.js"
+  }
+]);
